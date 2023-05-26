2e954431b6eb985a6f22704fe8edb6c564889900
kevoecodes <kelvinremmy01@gmail.com>
2023-05-26 19:23:58 +0300

    First Commit

--- pet_backend/asgi.py
+++ pet_backend/asgi.py
@@ -2,6 +2,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ioms_backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pet_backend.settings")
 django.setup()
 application = get_default_application()
